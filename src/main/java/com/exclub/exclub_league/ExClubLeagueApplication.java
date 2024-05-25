1aa3dd2c490c46fc8f5893cd16d75d4c905cb028
yulimlvphs <iojl1202@naver.com>
2024-05-25 19:48:59 +0900

    first commit

---
+++ src/main/java/com/exclub/exclub_league/ExClubLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.exclub.exclub_league;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExClubLeagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExClubLeagueApplication.class, args);
+    }
+
+}
